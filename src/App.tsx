e0f09ce715ba2cc8e0e5ff52f1b9c558621bd825
cleopatraropot <cleopatraropot@gmail.com>
2025-05-07 14:08:02 +0300

    update quiz list UI with numeric IDs, emoji hints, and vibrant 3D card design

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SelectQuiz from './pages/SelectQuiz';
+import QuizList from "./pages/QuizList";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/select-quiz" element={<SelectQuiz />} />
+            <Route path="/quiz-list/:category" element={<QuizList />} />
         </Routes>
       </Router>
   );
